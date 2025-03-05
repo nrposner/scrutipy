27262d4b84d9c627f39845be7487eb5cfb987271
nrposner <nicolasposner@gmail.com>
2025-03-05 15:56:18 -0600

    grim_probability function online

---
+++ src/grim_stats.rs
@@ -0,0 +1,37 @@
+#[allow(unused_imports)]
+use crate::utils::{decimal_places_scalar, dustify, reround};
+
+pub fn grim_probability(x: &str, n: u32, items: u32, percent: bool) -> f64 {
+    let mut digits: i32 = decimal_places_scalar(Some(x), ".").unwrap();
+
+    if percent {
+        digits += 2
+    };
+
+    let p10 = 10.0f64.powi(digits);
+
+    f64::max((p10 - (n as f64 * items as f64)) / p10, 0.0f64)
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    pub fn grim_probability_test_1() {
+        let val = grim_probability("8.2", 6, 1, true);
+        assert_eq!(val, 0.994)
+    }
+
+    #[test]
+    pub fn grim_probability_test_2() {
+        let val = grim_probability("6.7", 9, 1, false);
+        assert_eq!(val, 0.1)
+    }
+
+    #[test]
+    pub fn grim_probability_test_3() {
+        let val = grim_probability("3.333", 3, 3, false);
+        assert_eq!(val, 0.991)
+    }
+}
