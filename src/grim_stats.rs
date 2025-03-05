8f9b15246df6330ecdb14f57a9cc7792adfde343
nrposner <nicolasposner@gmail.com>
2025-03-05 16:20:04 -0600

    grim_total function online

--- src/grim_stats.rs
+++ src/grim_stats.rs
@@ -25,6 +25,18 @@ pub fn grim_ratio(x: &str, n: u32, items: u32, percent: bool) -> f64 {
     (p10 - (n as f64 * items as f64)) / p10
 }
 
+pub fn grim_total(x: &str, n: u32, items: u32, percent: bool) -> i32 {
+    let mut digits: i32 = decimal_places_scalar(Some(x), ".").unwrap();
+
+    if percent {
+        digits += 2
+    };
+
+    let p10 = 10.0f64.powi(digits);
+
+    (p10 - (n as f64 * items as f64)).floor() as i32
+}
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
@@ -75,5 +87,29 @@ pub mod tests {
     pub fn grim_ratio_test_4() {
         let val = grim_ratio("60.7", 9, 7, false);
         assert_eq!(val, -5.3)
+    }
+
+    #[test]
+    pub fn grim_total_test_1() {
+        let val = grim_total("8.2", 6, 1, true);
+        assert_eq!(val, 994)
+    }
+
+    #[test]
+    pub fn grim_total_test_2() {
+        let val = grim_total("6.7", 9, 1, false);
+        assert_eq!(val, 1)
+    }
+
+    #[test]
+    pub fn grim_total_test_3() {
+        let val = grim_total("3.333", 3, 3, false);
+        assert_eq!(val, 991)
+    }
+
+    #[test]
+    pub fn grim_total_test_4() {
+        let val = grim_total("60.7", 9, 7, false);
+        assert_eq!(val, -53)
     }
 }
