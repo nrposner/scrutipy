6147a483579051149b7be651d1c0d1f663029fdc
nrposner <nicolasposner@gmail.com>
2025-03-24 18:14:07 -0500

    added simple test for grim_map_df

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,7 @@ import scrutipy_rs
 from scrutipy_rs import grim_scalar
 from scrutipy_rs import grim_map_df
 import pandas as pd 
+import polars as pl
 
 def test_grim_1():
     result = grim_scalar("5.19", 40)
@@ -14,8 +15,8 @@ def test_grim_2():
 
 def test_grim_map_1():
 
-    df = pd.read_csv("data/pigs1.csv")
+    df = pl.read_csv("data/pigs1.csv")
 
-    bools, errors = grim_map_df(df)
+    bools, errors = grim_map_df(df, 1, 2)
 
-    assert bools == list(True, False, False, False, False, True, False, True, False, False, True, False)
+    assert bools == list([True, False, False, False, False, True, False, True, False, False, True, False])
