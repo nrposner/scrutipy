d53afcbe018d21798beaac1d6271618c582731f4
nrposner <nicolasposner@gmail.com>
2025-03-24 17:48:43 -0500

    added grim_map_df to pyfunctions

--- src/pyfunctions.rs
+++ src/pyfunctions.rs
@@ -1,5 +1,6 @@
 use crate::grim::grim_scalar;
 use crate::grimmer::grimmer;
+use crate::grim_map_df::grim_map_df;
 use pyo3::prelude::Bound;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
@@ -9,5 +10,6 @@ use pyo3::wrap_pyfunction;
 fn scrutipy_rs(module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(grim_scalar, module)?)?;
     module.add_function(wrap_pyfunction!(grimmer, module)?)?;
+    module.add_function(wrap_pyfunction!(grim_map_df, module)?)?;
     Ok(())
 }
