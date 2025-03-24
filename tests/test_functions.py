70c3363bdcdea0c453fd9d968917451650a9705d
nrposner <nicolasposner@gmail.com>
2025-03-24 17:56:21 -0500

    prototyping pytest for grim_map_df

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 import scrutipy_rs
 from scrutipy_rs import grim_scalar
-
+from scrutipy_rs import grim_map_df
+import pandas as pd 
 
 def test_grim_1():
     result = grim_scalar("5.19", 40)
@@ -10,3 +11,11 @@ def test_grim_1():
 def test_grim_2():
     result = grim_scalar(5.19, 40)
     assert not result
+
+def test_grim_map_1():
+
+    df = pd.read_csv("data/pigs1.csv")
+
+    bools, errors = grim_map_df(df)
+
+    assert bools == list(True, False, False, False, False, True, False, True, False, False, True, False)
