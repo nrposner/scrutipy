e3b22998ef66e858d160ee06bbf014493bd9334e
nrposner <nicolasposner@gmail.com>
2025-04-09 16:12:47 -0500

    added debit_map documentation

--- src/debit_map.rs
+++ src/debit_map.rs
@@ -8,6 +8,62 @@ use pyo3::prelude::*;
 use pyo3::exceptions::PyImportError;
 use crate::debit_map_df::debit_map_pl;
 
+/// Computes consistency using the Descriptive Binary test (DEBIT) for means and standard
+/// standard deviations
+///
+/// This function leverages the `polars` library to process a pandas DataFrame 
+/// and compute consistency based on the provided formula and rounding methods.
+/// It returns a tuple containing a list of boolean values indicating 
+/// consistency and an optional list of indices where errors occurred.
+///
+/// Parameters:
+/// - `pandas_df` (DataFrame): The input pandas DataFrame to be processed.
+/// - `x_col` (ColumnInput): The column index or name for the x values. Defaults
+/// to the first column.
+/// - `sd_col` (ColumnInput): The column index or name for the standard 
+/// deviation values. Defaults to the second column.
+/// - `n_col` (ColumnInput): The column index or name for the sample sizes. 
+/// Defaults to the third column.
+/// - `formula` (str): The formula to use for computation. Defaults to "mean_n".
+/// - `rounding` (str): The rounding method to apply. Defaults to "up_or_down".
+/// - `threshold` (float): The threshold value for rounding. Defaults to 5.0.
+/// - `show_rec` (bool): Whether to show reconstructed values. Defaults to False.
+/// - `symmetric` (bool): Whether the rounding should be symmetric. Defaults to False.
+/// - `silence_default_warning` (bool): Suppress warnings about default column 
+/// usage. Defaults to False.
+/// - `silence_numeric_warning` (bool): Suppress warnings about numeric issues. 
+/// Defaults to False.
+///
+/// Returns:
+/// - tuple: A tuple containing:
+///     - List[bool]: A list indicating the consistency of each row.
+///     - Optional[List[int]]: An optional list of indices where inconsistencies
+///     were found.
+///
+/// Raises:
+/// - ImportError: If the `polars` package is not installed.
+///
+/// Warnings:
+/// - A warning is issued if the default column indices are used without 
+/// modification, unless `silence_default_warning` is set to True.
+/// - A warning is issued if the xs and sds columns are not in string form 
+/// in order to preserve floating zeros, unless `silence_numerical_warning`
+/// is set to True
+///
+/// Example:
+/// ```python
+/// import pandas as pd
+/// from scrutipy import debit_map
+///
+/// df = pd.DataFrame({
+///     'x': [0.36, 0.11],
+///     'sd': [0.11, 0.31],
+///     'n': [20, 40]
+/// })
+///
+/// result, errors = debit_map(df)
+/// print(result)  # Output: [True, False]
+/// print(errors)  # Output: None (as no errors occurred)
 #[allow(clippy::too_many_arguments)]
 #[pyfunction(signature = (
      pandas_df, 
