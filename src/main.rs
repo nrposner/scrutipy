de19dcc85f72c3b241f89469540e962a2219f9b1
nrposner <nicolasposner@gmail.com>
2025-08-26 19:18:51 +0200

    added closure module

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ pub mod sd_binary;
 pub mod utils;
 //use std::env;
 pub mod debit;
+pub mod closure;
 
 #[allow(unused_imports)]
 use crate::debit::debit_scalar;
 
 // use in future, commented out for the moment
 //env::set_var("RUST_BACKTRACE", "1");
-fn main() {
-}
+fn main() { }
